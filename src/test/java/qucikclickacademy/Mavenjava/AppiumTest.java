227aa05ec793c0874720f8f8a2f24cb376077e41
chottu100 <chottu100@yahoo.com>
2020-06-09 15:38:37 -0300

    this is for integration with Jenkins

---
+++ src/test/java/qucikclickacademy/Mavenjava/AppiumTest.java
@@ -0,0 +1,14 @@
+package qucikclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void appiumtest()
+	{
+		System.out.println("this is a test for Appium");
+		System.out.println("Change made in Develop branch");
+	}
+
+}
---
+++ src/test/java/qucikclickacademy/Mavenjava/SeleniumTest.java
@@ -0,0 +1,25 @@
+package qucikclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	
+	public void browserAutomation()
+	{
+		System.out.println("this is a test with Maven");
+
+		System.out.println("Added to test GIT and GITHUB");
+
+		System.out.println("change made in Gutstuff");
+		
+		System.out.println("try again gitproj");
+		
+		System.out.println("Change made in Develop branch");
+		
+		System.out.println("Change made in Develop branch in gitproj");
+
+	}
+
+}
